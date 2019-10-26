18ffd4ae7666b7cb9c86ca452d9f3d5cdda05830
しょうぐん <syougun360@infoseek.jp>
2019-10-26 21:27:59 +0900

    update player jump control

--- VGA_Project_Game/Assets/Scripts/PlayerJumpControl.cs
+++ VGA_Project_Game/Assets/Scripts/PlayerJumpControl.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerJumpControl : MonoBehaviour
 {
+    [SerializeField]
+    Transform _transform = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _transform = transform;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // TODO: test   
+    }
+
+    Transform GetTransform()
+    {
+        return _transform;
     }
 }
