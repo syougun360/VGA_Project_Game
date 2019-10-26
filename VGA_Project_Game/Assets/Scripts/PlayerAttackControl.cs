db003cc23e9d729333fd6effc60281e55dc36c83
しょうぐん <syougun360@infoseek.jp>
2019-10-26 21:49:39 +0900

    update

--- VGA_Project_Game/Assets/Scripts/PlayerAttackControl.cs
+++ VGA_Project_Game/Assets/Scripts/PlayerAttackControl.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAttackControl : MonoBehaviour
+public class PlayerAttackControl
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
